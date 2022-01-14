57a6b77052017be57949559fba4c16e18946c7b1
Piero G <piero.guerrero@gmail.com>
2022-01-13 21:51:21 -0500

    helpers 2

--- examples/lib/helper.js
+++ examples/lib/helper.js
@@ -26,3 +26,44 @@ function getRandomBetween(start, end) {
         return Math.floor(Math.random() * (end - start + 1) + start);
     }
 }
+
+//CHECK IF A NUMBER IS PRIME OR NOT
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+//FOR A SINGLE WORD OR PHRASE GET THE FIRST LETTER IN UPPER CASE AND THE REST IN LOWER 
+function getCapitalizedWord(text = "") {
+    return (typeof text === 'string') ? text.charAt(0).toUpperCase() + text.slice(1).toLowerCase() : "";
+}
+
+function getFizzBuzz(n) {
+    if (Number.isInteger(n)) {
+
+        // let nFizzBuzz = "";
+
+        // if (n % 3 === 0) {
+        //     nFizzBuzz = "Fizz";
+        // }
+
+        // if (n % 5 === 0) {
+        //     nFizzBuzz += "Buzz";
+        // }
+
+        // return (nFizzBuzz.length > 0) ? nFizzBuzz : n.toString();
+
+        if (n % 3 === 0 && n % 5 === 0) {
+            return nFizzBuzz="FizzBuzz";
+        } else if (n % 3 === 0) {
+            return nFizzBuzz="Fizz";
+        } else if (n % 5 === 0) {
+            return nFizzBuzz="Buzz";
+        } else {
+            return n.toString()
+        }
+
+    } else return;
+}
